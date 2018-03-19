5c356978555aa7853559770bfba8ca2b2a44a5a9
Marco Leong <marcogood411@gmail.com>
2018-03-20 00:19:00 +0800

    Fix extension class issue

--- USJGSuiteBundle.php
+++ USJGSuiteBundle.php
@@ -5,8 +5,12 @@ namespace USJ\GSuiteBundle;
 
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use USJ\GSuiteBundle\DependencyInjection\USJGSuiteExtension;
 
 class USJGSuiteBundle extends Bundle
 {
-
+    public function getContainerExtension()
+    {
+        return new USJGSuiteExtension();
+    }
 }
